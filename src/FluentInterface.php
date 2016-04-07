769d255fc2cdb9b87547ef782c2d8fd06be4cc9d
Philippe Assis <assis@philippeassis.com>
2016-04-07 13:32:37 -0300

    version

---
+++ src/FluentInterface.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philippe
+ * Date: 31/03/16
+ * Time: 14:25
+ */
+
+namespace Wiidoo\Support;
+
+
+class FluentInterface extends Treatments
+{
+
+    public function __call($name, $arguments)
+    {
+        if ($this->validatePropertyChange($name)) {
+            if (isset($arguments[0])) {
+                $this->$name = $arguments[0];
+            } elseif (is_bool($this->$name)) {
+                $this->$name = !$this->searchNegatedName($name, true);
+            }
+
+        } elseif ($negatedName = $this->searchNegatedName($name)) {
+            $this->$negatedName = false;
+        }
+
+        return $this;
+    }
+
+    protected function searchNegatedName($name, $returnBool = false)
+    {
+        $prefixes = ['no', 'not', 'disable'];
+
+        do {
+            $prefix = current($prefixes);
+
+            if (strripos($name, $prefix) === 0) {
+                if ($returnBool) {
+                    return true;
+                }
+
+                $realName = lcfirst(str_replace($prefix, '', $name));
+
+                if (property_exists($this->classCall, $realName)) {
+                    return $realName;
+                }
+            }
+
+        } while (next($prefixes));
+
+        return false;
+    }
+
+}
---
+++ src/Treatments.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philippe
+ * Date: 31/03/16
+ * Time: 15:01
+ */
+
+namespace Wiidoo\Support;
+
+use Illuminate\Support\Facades\Config;
+
+class Treatments
+{
+    const IS_PUBLIC = false;
+
+    const IS_PUBLIC_AND_PROTECTED = true;
+
+    public $classCall;
+
+    public $config;
+
+    public function __construct()
+    {
+        $this->classCall = get_called_class();
+    }
+
+    protected function mergeConfig($name)
+    {
+        if (Config::get($name)) {
+            $config = Config::get($name);
+
+            foreach ($config as $key => $value) {
+                if ($this->validatePropertyChange($key, true)) {
+                    $this->$key = $value;
+                }
+            }
+        }
+    }
+
+    protected function validatePropertyChange($name, $protected = false)
+    {
+        if (property_exists($this->classCall, $name)) {
+
+            $property = new \ReflectionClass($this->classCall);
+            $property = $property->getProperty($name);
+
+            return $property->isPublic() ? true : ($protected ? $property->isProtected() : false);
+        }
+
+        return false;
+    }
+
+}
